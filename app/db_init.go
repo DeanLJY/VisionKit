e101106737453304243e89d04b5a75cf5cc6b2ba
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2022-08-29 22:06:08 +0000

    Наслаждайся вэб радио - Enjoy web radio

--- app/db_init.go
+++ app/db_init.go
@@ -22,3 +22,58 @@ func InitDB(init bool) {
 			data_type TEXT,
 			metadata TEXT DEFAULT '',
 			-- only set if computed
+			hash TEXT,
+			done INTEGER DEFAULT 1
+		)`)
+		db.Exec(`CREATE TABLE IF NOT EXISTS annotate_datasets (
+			id INTEGER PRIMARY KEY ASC,
+			dataset_id INTEGER REFERENCES datasets(id),
+			inputs TEXT,
+			tool TEXT,
+			params TEXT
+		)`)
+		db.Exec(`CREATE TABLE IF NOT EXISTS pytorch_archs (
+			id TEXT PRIMARY KEY,
+			params TEXT
+		)`)
+		db.Exec(`CREATE TABLE IF NOT EXISTS pytorch_components (
+			id TEXT PRIMARY KEY,
+			params TEXT
+		)`)
+		db.Exec(`CREATE TABLE IF NOT EXISTS exec_nodes (
+			id INTEGER PRIMARY KEY ASC,
+			name TEXT,
+			op TEXT,
+			params TEXT,
+			parents TEXT,
+			workspace TEXT
+		)`)
+		db.Exec(`CREATE TABLE IF NOT EXISTS exec_ds_refs (
+			node_id INTEGER,
+			dataset_id INTEGER,
+			UNIQUE(node_id, dataset_id)
+		)`)
+		db.Exec(`CREATE TABLE IF NOT EXISTS workspaces (
+			name TEXT PRIMARY KEY
+		)`)
+		db.Exec(`CREATE TABLE IF NOT EXISTS ws_datasets (
+			dataset_id INTEGER,
+			workspace TEXT,
+			UNIQUE(dataset_id, workspace)
+		)`)
+		db.Exec(`CREATE TABLE IF NOT EXISTS jobs (
+			id INTEGER PRIMARY KEY ASC,
+			name TEXT,
+			-- e.g. 'execnode'
+			type TEXT,
+			-- how to process the job output and render the job
+			op TEXT,
+			metadata TEXT,
+			start_time TIMESTAMP,
+			state TEXT DEFAULT '',
+			done INTEGER DEFAULT 0,
+			error TEXT DEFAULT ''
+		)`)
+
+		// add missing pytorch components
+		componentPath := "python/skyhook/pytorch/com
