c753767a21e09343f6611712b76e196fa16fdd2e
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2022-10-21 22:51:22 +0000

    x86, mm, hugetlb: Add missing TLB page invalidation for hugetlb_cow()

--- app/exec_db.go
+++ app/exec_db.go
@@ -17,4 +17,53 @@ const ExecNodeQuery = "SELECT id, name, op, params, parents, workspace FROM exec
 
 func execNodeListHelper(rows *Rows) []*DBExecNode {
 	nodes := []*DBExecNode{}
-	for rows.Next
+	for rows.Next() {
+		var node DBExecNode
+		var parentsRaw string
+		rows.Scan(&node.ID, &node.Name, &node.Op, &node.Params, &parentsRaw, &node.Workspace)
+		skyhook.JsonUnmarshal([]byte(parentsRaw), &node.Parents)
+		if node.Parents == nil {
+			node.Parents = make(map[string][]skyhook.ExecParent)
+		}
+
+		// make sure parents list is set for each input
+		for _, input := range node.GetInputs() {
+			if node.Parents[input.Name] != nil {
+				continue
+			}
+			node.Parents[input.Name] = []skyhook.ExecParent{}
+		}
+
+		nodes = append(nodes, &node)
+	}
+	return nodes
+}
+
+func ListExecNodes() []*DBExecNode {
+	rows := db.Query(ExecNodeQuery)
+	return execNodeListHelper(rows)
+}
+
+func (ws DBWorkspace) ListExecNodes() []*DBExecNode {
+	rows := db.Query(ExecNodeQuery + " WHERE workspace = ?", ws)
+	return execNodeListHelper(rows)
+}
+
+func GetExecNode(id int) *DBExecNode {
+	rows := db.Query(ExecNodeQuery + " WHERE id = ?", id)
+	nodes := execNodeListHelper(rows)
+	if len(nodes) == 1 {
+		return nodes[0]
+	} else {
+		return nil
+	}
+}
+
+func NewExecNode(name string, op string, params string, parents map[string][]skyhook.ExecParent, workspace string) *DBExecNode {
+	res := db.Exec(
+		"INSERT INTO exec_nodes (name, op, params, parents, workspace) VALUES (?, ?, ?, ?, ?)",
+		name, op, params,
+		string(skyhook.JsonMarshal(parents)),
+		workspace,
+	)
+	node := GetExecNode(res.La
