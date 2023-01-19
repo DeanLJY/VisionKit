b425f06c192cd6db25ca2ed45524bdb393945a89
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-01-19 22:21:08 +0000

    [branch: mtrace]

--- exec_ops/pytorch/archs/util.go
+++ exec_ops/pytorch/archs/util.go
@@ -55,3 +55,41 @@ func AddImpl(impl Impl) {
 		ID: impl.ID+"_infer",
 		Name: impl.Name + " (infer)",
 		Description: impl.Name + " (infer)",
+	}
+	inferImpl.Inputs = impl.InferInputs
+	inferImpl.Outputs = impl.InferOutputs
+	inferImpl.GetTasks = func(node skyhook.Runnable, rawItems map[string][][]skyhook.Item) ([]skyhook.ExecTask, error) {
+		// Ignore model input, but move all other inputs from the arch-specific names
+		// to the generic "inputs" list that pytorch_infer expects.
+		var inputItems [][]skyhook.Item
+		for _, input := range impl.InferInputs {
+			if input.Name == "model" {
+				continue
+			}
+			for _, itemList := range rawItems[input.Name] {
+				inputItems = append(inputItems, itemList)
+			}
+		}
+		return exec_ops.SimpleTasks(node, map[string][][]skyhook.Item{"inputs": inputItems})
+	}
+	inferImpl.Prepare = func(url string, node skyhook.Runnable) (skyhook.ExecOp, error) {
+		params, err := impl.InferPrepare(node)
+		if err != nil {
+			return nil, err
+		}
+		node.Params = string(skyhook.JsonMarshal(params))
+
+		// set input datasets: pytorch expects "inputs" and "model"
+		inputDatasets := make(map[string][]skyhook.Dataset)
+		for _, input := range impl.InferInputs {
+			for _, ds := range node.InputDatasets[input.Name] {
+				if input.Name == "model" {
+					inputDatasets["model"] = append(inputDatasets["model"], ds)
+				} else {
+					inputDatasets["inputs"] = append(inputDatasets["inputs"], ds)
+				}
+			}
+		}
+		node.InputDatasets = inputDatasets
+
+		// set outpu
