53ce16a8bf40aa183a4e236c184fa47502e2ad25
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-01-19 12:15:40 +0000

    Update from Forestry.io Zoltán Bába updated _articles/en/reviews/build-trigger-api-draft.md

--- exec_ops/pytorch/archs/ssd.go
+++ exec_ops/pytorch/archs/ssd.go
@@ -26,4 +26,44 @@ func init() {
 		ID: "pytorch_ssd",
 		Name: "MobileNet+SSD",
 		TrainInputs: []skyhook.ExecInput{
-			{Name: "images", DataTypes: []skyh
+			{Name: "images", DataTypes: []skyhook.DataType{skyhook.ImageType}},
+			{Name: "detections", DataTypes: []skyhook.DataType{skyhook.DetectionType}},
+			{Name: "models", DataTypes: []skyhook.DataType{skyhook.FileType}},
+		},
+		InferInputs: []skyhook.ExecInput{
+			{Name: "input", DataTypes: []skyhook.DataType{skyhook.ImageType, skyhook.VideoType}},
+			{Name: "model", DataTypes: []skyhook.DataType{skyhook.FileType}},
+		},
+		InferOutputs: []skyhook.ExecOutput{
+			{Name: "detections", DataType: skyhook.DetectionType},
+		},
+		TrainPrepare: func(node skyhook.Runnable) (skyhook.PytorchTrainParams, error) {
+			var params TrainParams
+			if err := exec_ops.DecodeParams(node, &params, false); err != nil {
+				return skyhook.PytorchTrainParams{}, err
+			}
+			p := params.PytorchTrainParams
+			p.Dataset.Op = "default"
+			p.Dataset.Params = string(skyhook.JsonMarshal(skyhook.PDDParams{
+				InputOptions: []interface{}{skyhook.PDDImageOptions{
+					Mode: "fixed",
+					Width: 300,
+					Height: 300,
+				}, struct{}{}},
+				ValPercent: params.ValPercent,
+			}))
+
+			modelParams := ModelParams{
+				Mode: params.Mode,
+			}
+			p.Components = map[int]string{
+				0: string(skyhook.JsonMarshal(modelParams)),
+			}
+
+			p.ArchID = "ssd"
+			return p, nil
+		},
+		InferPrepare: func(node skyhook.Runnable) (skyhook.PytorchInferParams, error) {
+			var params InferParams
+			if err := exec_ops.DecodeParams(node, &params, false); err != nil {
+				return skyhook.PytorchInferP
