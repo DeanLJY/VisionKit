ad0875ab9c5020bfac545893f74ef626dfe80233
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-02-03 22:06:25 +0000

    standard style

--- exec_ops/pytorch/archs/yolov5.go
+++ exec_ops/pytorch/archs/yolov5.go
@@ -33,4 +33,44 @@ func init() {
 			{Name: "models", DataTypes: []skyhook.DataType{skyhook.FileType}},
 		},
 		InferInputs: []skyhook.ExecInput{
-		
+			{Name: "input", DataTypes: []skyhook.DataType{skyhook.ImageType, skyhook.VideoType}},
+			{Name: "model", DataTypes: []skyhook.DataType{skyhook.FileType}},
+		},
+		InferOutputs: []skyhook.ExecOutput{
+			{Name: "detections", DataType: skyhook.DetectionType},
+		},
+		TrainPrepare: func(node skyhook.Runnable) (skyhook.PytorchTrainParams, error) {
+			var params TrainParams
+			if err := exec_ops.DecodeParams(node, &params, false); err != nil {
+				return skyhook.PytorchTrainParams{}, err
+			}
+			p := params.PytorchTrainParams
+			p.Dataset.Op = "default"
+			p.Dataset.Params = string(skyhook.JsonMarshal(skyhook.PDDParams{
+				InputOptions: []interface{}{params.Resize, struct{}{}},
+				ValPercent: params.ValPercent,
+			}))
+
+			modelParams := ModelParams{
+				Mode: params.Mode,
+			}
+			p.Components = map[int]string{
+				0: string(skyhook.JsonMarshal(modelParams)),
+			}
+
+			p.ArchID = "yolov5"
+			return p, nil
+		},
+		InferPrepare: func(node skyhook.Runnable) (skyhook.PytorchInferParams, error) {
+			var params InferParams
+			if err := exec_ops.DecodeParams(node, &params, false); err != nil {
+				return skyhook.PytorchInferParams{}, err
+			}
+			p := skyhook.PytorchInferParams{
+				ArchID: "yolov5",
+				OutputDatasets: []skyhook.PIOutputDataset{{
+					ComponentIdx: 0,
+					Layer: "detections",
+					DataType: skyhook.DetectionType,
+				}},
+				InputOptions: []sk
