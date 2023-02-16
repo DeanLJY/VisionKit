50f365687f2371bb99a21cb84994a759fcad5fe2
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-02-16 06:22:26 +0000

    Updates auto-save

--- exec_ops/pytorch/pytorch.go
+++ exec_ops/pytorch/pytorch.go
@@ -1,1 +1,58 @@
-package pyt
+package pytorch
+
+import (
+	"github.com/skyhookml/skyhookml/skyhook"
+
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func GetTrainArgs(url string, archID string) (*skyhook.PytorchArch, map[string]*skyhook.PytorchComponent, error) {
+	// get the PytorchComponents
+	var arch skyhook.PytorchArch
+	err := skyhook.JsonGet(url, fmt.Sprintf("/pytorch/archs/%s", archID), &arch)
+	if err != nil {
+		return nil, nil, err
+	}
+	components := make(map[string]*skyhook.PytorchComponent)
+	for _, compSpec := range arch.Params.Components {
+		if components[compSpec.ID] != nil {
+			continue
+		}
+		var comp skyhook.PytorchComponent
+		err := skyhook.JsonGet(url, fmt.Sprintf("/pytorch/components/%s", compSpec.ID), &comp)
+		if err != nil {
+			return nil, nil, err
+		}
+		components[comp.ID] = &comp
+	}
+
+	return &arch, components, nil
+}
+
+// Download this repository to the models/ folder if it doesn't already exist
+func EnsureRepository(repo skyhook.PytorchRepository) error {
+	hash := repo.Hash()
+
+	// does it already exist?
+	path := filepath.Join("data/models", hash)
+	if _, err := os.Stat(path); err == nil {
+		return nil
+	}
+
+	// clone the repository
+	log.Printf("[pytorch] cloning repository %s@%s", repo.URL, repo.Commit)
+	cmd := exec.Command(
+		"git", "clone", repo.URL, path,
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	if repo.Commit != "" {
+		cmd = ex
