8c6cddafe31d19da06030010e713f76f292fa021
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-03-21 06:56:43 +0000

    [l10n] Update Japanese translation

--- exec_ops/pytorch/train.py
+++ exec_ops/pytorch/train.py
@@ -49,4 +49,40 @@ if params.get('Components', None):
 			comp_params = json.loads(comp_spec['Params'])
 		if overwrite_comp_params.get(comp_idx, None):
 			comp_params.update(json.loads(overwrite_comp_params[comp_idx]))
-		comp_spec['Params'] = json.dump
+		comp_spec['Params'] = json.dumps(comp_params)
+
+device = torch.device('cuda:0')
+#device = torch.device('cpu')
+
+# get train and val Datasets
+print('loading datasets')
+dataset_provider = skyhook_dataset.providers[params['Dataset']['Op']]
+dataset_params = json.loads(params['Dataset']['Params'])
+train_set, val_set = dataset_provider(url, datasets, dataset_params, train_split, valid_split)
+datatypes = train_set.get_datatypes()
+
+# get data augmentation steps
+# this is a list of objects that provide forward() function
+# we will apply the forward function on batches from DataLoader
+print('loading data augmentations')
+ds_augments = []
+torch_augments = []
+for spec in params['Augment']:
+	cls_func = skyhook_augment.augmentations[spec['Op']]
+	obj = cls_func(json.loads(spec['Params']), datatypes)
+	if obj.pre_torch:
+		ds_augments.append(obj)
+	else:
+		torch_augments.append(obj)
+
+train_set.set_augments(ds_augments)
+val_set.set_augments(ds_augments)
+
+# apply data augmentation on validation set
+# this is because some augmentations are random but we want a consistent validation set
+# here we assume the validation set fits in system memory, but not necessarily GPU memory
+# so we apply augmentation on CPU, whereas during training we will apply on GPU
+
+train_params = json.loads(params['Train']['Params'])
+
+print('prepa
