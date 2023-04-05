a8d1b857f2437de24b56ccfab316b970e03cb6a3
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-04-05 10:31:36 +0000

    yikes

--- exec_ops/resample/resample.go
+++ exec_ops/resample/resample.go
@@ -60,4 +60,56 @@ func (e *Resample) Apply(task skyhook.ExecTask) error {
 					"provider_info": {item.Fname()},
 				}, nil)
 			} else {
-				// Filename should always be available since we shouldn't be loading video into m
+				// Filename should always be available since we shouldn't be loading video into memory.
+				return fmt.Errorf("cannot resample video item that is not available on disk")
+			}
+		}
+
+		// re-sample by building via Writer
+		// if not video, the input must be sequence type
+		data, metadata, err := item.LoadData()
+		if err != nil {
+			return err
+		}
+		spec := item.DataSpec().(skyhook.SequenceDataSpec)
+
+		outItem, err := exec_ops.AddItem(e.URL, dataset, task.Key, item.Ext, item.Format, metadata)
+		if err != nil {
+			return err
+		}
+		writer := outItem.LoadWriter()
+
+		outputLength := spec.Length(data) * fraction[0] / fraction[1]
+		for i := 0; i < outputLength; i++ {
+			idx := i*fraction[1]/fraction[0]
+			writer.Write(spec.Slice(data, idx, idx+1))
+		}
+
+		return writer.Close()
+	}
+
+	for i, itemList := range task.Items["inputs"] {
+		err := process(itemList[0], e.Datasets[fmt.Sprintf("outputs%d", i)])
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (e *Resample) Close() {}
+
+func init() {
+	skyhook.AddExecOpImpl(skyhook.ExecOpImpl{
+		Config: skyhook.ExecOpConfig{
+			ID: "resample",
+			Name: "Resample",
+			Description: "Resample sequence data at a different rate",
+		},
+		Inputs: []skyhook.ExecInput{{Name: "inputs", Variable: true}},
+		GetOutputs: exec_ops.GetOutputsSimilarToInputs,
+		Requirements: func(node skyhook.Runnable) map[string]int {
+			return nil
+		},
+		Get
