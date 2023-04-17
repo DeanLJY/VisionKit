21f12b869fa03f80a9288387f65499d2f46c59c3
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-04-17 12:21:28 +0000

    I18N: Update translation (French)

--- exec_ops/union/union.go
+++ exec_ops/union/union.go
@@ -27,4 +27,44 @@ func init() {
 		},
 		GetTasks: func(node skyhook.Runnable, rawItems map[string][][]skyhook.Item) ([]skyhook.ExecTask, error) {
 			// Create one task per item.
-			// We set the Key of the task to the output key
+			// We set the Key of the task to the output key that we want to write as.
+			// This may not match the input key in the case that there are duplicate keys across input datasets.
+			seenKeys := make(map[string]bool)
+			var tasks []skyhook.ExecTask
+			for _, itemList := range rawItems["inputs"] {
+				for _, item := range itemList {
+					key := item.Key
+					for i := 1; seenKeys[key]; i++ {
+						key = item.Key + "-" + strconv.Itoa(i)
+					}
+					seenKeys[key] = true
+					tasks = append(tasks, skyhook.ExecTask{
+						Key: key,
+						Items: map[string][][]skyhook.Item{"inputs": {{item}}},
+					})
+				}
+			}
+			return tasks, nil
+		},
+		Prepare: func(url string, node skyhook.Runnable) (skyhook.ExecOp, error) {
+			applyFunc := func(task skyhook.ExecTask) error {
+				item := task.Items["inputs"][0][0]
+				outDataset := node.OutputDatasets["output"]
+				err := skyhook.JsonPostForm(url, fmt.Sprintf("/datasets/%d/items", outDataset.ID), urllib.Values{
+					"key": {task.Key},
+					"ext": {item.Ext},
+					"format": {item.Format},
+					"metadata": {item.Metadata},
+					"provider": {"reference"},
+					"provider_info": {item.Fname()},
+				}, nil)
+				if err != nil {
+					return err
+				}
+				return nil
+			}
+			return skyhook.SimpleExecOp{ApplyFunc: applyFunc}, nil
+		},
+		ImageName: "skyhookml/basic",
+	})
+}
---
+++ exec_ops/unsupervised_reid/preprocess_matches.go
@@ -0,0 +1,89 @@
+
+package reid
+
+import (
+	"github.com/skyhookml/skyhookml/skyhook"
+
+	"runtime"
+
+	"github.com/mitroadmaps/gomapinfer/common"
+)
+
+const MaxMatchAge = 10
+
+// Matches detections in a given frame forwards through frames to the maxLength.
+// Returns a map detectionIdx (in startFrame) -> set of (frame, detectionIdx in frame)
+func matchFrom(detections [][]skyhook.Detection, startFrame int, maxLength int) map[int]map[[2]int]bool {
+	toRect := func(d skyhook.Detection) common.Rectangle {
+		return common.Rectangle{
+			common.Point{float64(d.Left), float64(d.Top)},
+			common.Point{float64(d.Right), float64(d.Bottom)},
+		}
+	}
+
+	// from detection_idx in startFrame to list of matching tuples (frame idx, detection idx)
+	curMatches := make(map[int]map[[2]int]bool)
+	finalMatches := make(map[int]map[[2]int]bool)
+	for idx := range detections[startFrame] {
+		curMatches[idx] = make(map[[2]int]bool)
+		finalMatches[idx] = make(map[[2]int]bool)
+		curMatches[idx][[2]int{startFrame, idx}] = true
+		finalMatches[idx][[2]int{startFrame, idx}] = true
+	}
+
+	for frameIdx := startFrame+1; frameIdx <= startFrame+maxLength && frameIdx < len(detections); frameIdx++ {
+		// find the detections we need to match
+		checkSet := make(map[[2]int]bool)
+		for _, matches := range curMatches {
+			for t := range matches {
+				if frameIdx - t[0] >= MaxMatchAge {
+					delete(matches, t)
+					continue
+				}
+				checkSet[t] = true
+			}
+		}
+
+		// determine connections between those in checkSet and those in current frame
+		connections := make(map[[2]int][][2]int)
+		for left := range checkSet {
+			leftFrame, leftIdx := left[0], left[1]
+			for rightIdx := 0; rightIdx < len(detections[frameIdx]); rightIdx++ {
+				leftRect := toRect(detections[leftFrame][leftIdx])
+				rightRect := toRect(detections[frameIdx][rightIdx])
+				if leftRect.IOU(rightRect) < 0.1 {
+					continue
+				}
+				connections[left] = append(connections[left], [2]int{frameIdx, rightIdx})
+			}
+		}
+
+		for idx, matches := range curMatches {
+			for left := range matches {
+				for _, right := range connections[left] {
+					matches[right] = true
+					finalMatches[idx][right] = true
+				}
+			}
+		}
+	}
+
+	return finalMatches
+}
+
+// Computes plausible matchings between every detection and other detections forwards/backwards in time.
+// Returns map from (frame idx, detection idx, match length) -> list of plausible detection idx in (frame idx + match length)
+func PreprocessMatches(detections [][]skyhook.Detection, matchLengths []int) map[[3]int][]int {
+	matches := make(map[[3]int][]int)
+	matchLengthSet := make(map[int]bool)
+	for _, matchLength := range matchLengths {
+		matchLengthSet[matchLength] = true
+	}
+	maxLength := matchLengths[len(matchLengths)-1]
+
+	// multi-threaded pre-processing
+	// we process each start frame independently
+	nthreads := runtime.NumCPU()
+	ch := make(chan int)
+	donech := make(chan map[[3]int][]int)
+	for i := 0; i < nthreads; i++ {
