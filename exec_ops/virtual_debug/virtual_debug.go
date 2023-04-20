99b3f24c17e1109bb40b9ff87f0a701da4012f38
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-04-20 21:28:19 +0000

    tooltip

--- exec_ops/virtual_debug/virtual_debug.go
+++ exec_ops/virtual_debug/virtual_debug.go
@@ -14,4 +14,41 @@ import (
 )
 
 func init() {
-	skyhook.ItemProviders["virtual_debug"] = skyhook.VirtualProvider(func(item skyho
+	skyhook.ItemProviders["virtual_debug"] = skyhook.VirtualProvider(func(item skyhook.Item, data interface{}, metadata skyhook.DataMetadata) (interface{}, skyhook.DataMetadata, error) {
+		return data, metadata, nil
+	}, false)
+
+	skyhook.AddExecOpImpl(skyhook.ExecOpImpl{
+		Config: skyhook.ExecOpConfig{
+			ID: "virtual_debug",
+			Name: "Virtual Debug",
+			Description: "Op implementing identity function with a virtual provider",
+		},
+		Inputs: []skyhook.ExecInput{{Name: "inputs", Variable: true}},
+		GetOutputs: exec_ops.GetOutputsSimilarToInputs,
+		Requirements: func(node skyhook.Runnable) map[string]int {
+			return nil
+		},
+		GetTasks: exec_ops.SimpleTasks,
+		Prepare: func(url string, node skyhook.Runnable) (skyhook.ExecOp, error) {
+			applyFunc := func(task skyhook.ExecTask) error {
+				for i, itemList := range task.Items["inputs"] {
+					item := itemList[0]
+					dataset := node.OutputDatasets[fmt.Sprintf("outputs%d", i)]
+					err := skyhook.JsonPostForm(url, fmt.Sprintf("/datasets/%d/items", dataset.ID), urllib.Values{
+						"key": {task.Key},
+						"ext": {item.Ext},
+						"format": {item.Format},
+						"metadata": {item.Metadata},
+						"provider": {"virtual_debug"},
+						"provider_info": {string(skyhook.JsonMarshal(item))},
+					}, nil)
+					if err != nil {
+						return err
+					}
+				}
+				return nil
+			}
+			return skyhook.SimpleExecOp{ApplyFunc: applyFunc}, nil
+		},
+		ImageName: "skyhookm
