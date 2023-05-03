b0e4fac3bce82795c551b659f0021d9171144d98
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-05-03 09:48:56 +0000

    drm/radeon: fix hw contexts for SUMO2 asics

---
+++ ops/ops.go
@@ -0,0 +1,25 @@
+package ops
+
+import (
+	_ "github.com/skyhookml/skyhookml/exec_ops/concatenate"
+	_ "github.com/skyhookml/skyhookml/exec_ops/convert"
+	_ "github.com/skyhookml/skyhookml/exec_ops/cropresize"
+	_ "github.com/skyhookml/skyhookml/exec_ops/detection_filter"
+	_ "github.com/skyhookml/skyhookml/exec_ops/extract_polygons"
+	_ "github.com/skyhookml/skyhookml/exec_ops/filter"
+	_ "github.com/skyhookml/skyhookml/exec_ops/geoimage_to_image"
+	_ "github.com/skyhookml/skyhookml/exec_ops/geojson_to_shape"
+	_ "github.com/skyhookml/skyhookml/exec_ops/make_geoimage"
+	_ "github.com/skyhookml/skyhookml/exec_ops/materialize"
+	_ "github.com/skyhookml/skyhookml/exec_ops/python"
+	_ "github.com/skyhookml/skyhookml/exec_ops/pytorch"
+	_ "github.com/skyhookml/skyhookml/exec_ops/pytorch/archs"
+	_ "github.com/skyhookml/skyhookml/exec_ops/reid_tracker"
+	_ "github.com/skyhookml/skyhookml/exec_ops/render"
+	_ "github.com/skyhookml/skyhookml/exec_ops/resample"
+	_ "github.com/skyhookml/skyhookml/exec_ops/sample"
+	_ "github.com/skyhookml/skyhookml/exec_ops/segmentation_mask"
+	_ "github.com/skyhookml/skyhookml/exec_ops/simple_tracker"
+	_ "github.com/skyhookml/skyhookml/exec_ops/split"
+	_ "github.com/skyhookml/skyhookml/exec_ops/union"
+	_ "github.com/s
