394ec33707e433d2bb110a31a96e4b18389d9876
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-05-05 07:52:36 +0000

    avr32: fix out-of-range jump in large kernels

--- python/skyhook/pytorch/augment/crop.py
+++ python/skyhook/pytorch/augment/crop.py
@@ -44,4 +44,45 @@ class Crop(object):
 			if self.data_types[i] in ('image', 'video', 'array'):
 				width, height = inputs[0].shape[2], inputs[0].shape[1]
 				target_width = int(width * self.Width[0]) // self.Width[1]
-		
+				target_height = int(height * self.Height[0]) // self.Height[1]
+				sx = int(xoff * width)
+				sy = int(yoff * height)
+				ex = sx + target_width
+				ey = sy + target_height
+
+				ninputs = []
+				for input in inputs:
+					input = input[:, sy:ey, sx:ex]
+					ninputs.append(input)
+				batch[i] = ninputs
+
+			elif self.data_types[i] == 'shape':
+				# shapes can be arbitrary polygons
+				# so we don't really want to support it
+				raise Exception('crop is not supported on shape types')
+
+			elif self.data_types[i] == 'detection':
+				# we clip detection bboxes inside the crop rectangle
+				# but we also remove ones that are entirely outside the crop rectangle
+				ninputs = []
+				for input in inputs:
+					ndetections = []
+					for d in input['detections']:
+						cls, sx, sy, ex, ey = d
+						sx, sy = coord_transform(sx, sy)
+						ex, ey = coord_transform(ex, ey)
+						if ex < 0 or sx >= 1 or ey < 0 or sy >= 1:
+							continue
+						sx = clip(sx, 0, 1)
+						sy = clip(sy, 0, 1)
+						ex = clip(ex, 0, 1)
+						ey = clip(ey, 0, 1)
+						ndetections.append([cls, sx, sy, ex, ey])
+					ndetections = numpy.array(ndetections, dtype='float32')
+					ninputs.append({
+						'counts': ndetections.shape[0],
+						'detections': ndetections,
+					})
+				batch[i] = ninputs
+
+			# we assume that other data types like int, floats
