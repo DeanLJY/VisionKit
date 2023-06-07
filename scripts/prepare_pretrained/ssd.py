aa57f21101aba747e7a60d165e0910a35b51b1c8
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-06-07 09:36:58 +0000

    add react-router

--- scripts/prepare_pretrained/ssd.py
+++ scripts/prepare_pretrained/ssd.py
@@ -15,4 +15,35 @@ in_fname = sys.argv[2]
 out_fname = sys.argv[3]
 
 device = torch.device('cpu')
-ssd_path = os.path.joi
+ssd_path = os.path.join('.', 'data', 'models', hashlib.sha256(b'https://github.com/qfgaohao/pytorch-ssd.git').hexdigest())
+
+# get arch, comps
+with open('exec_ops/pytorch/archs/ssd.json', 'r') as f:
+    arch = json.load(f)
+with open('python/skyhook/pytorch/components/ssd.json', 'r') as f:
+    comps = {'ssd': {'ID': 'ssd', 'Params': json.load(f)}}
+
+# set mode
+comp_params = json.loads(arch['Components'][0].get('Params', '{}'))
+comp_params['mode'] = mode
+arch['Components'][0]['Params'] = json.dumps(comp_params)
+
+# example inputs
+im_data = numpy.zeros((300, 300, 3), dtype='uint8')
+detection_data = [{'Left': 100, 'Right': 100, 'Top': 100, 'Bottom': 100}]
+# Need to repeat the inputs twice because SSD requires batch_size>1 for normalization.
+example_inputs = [
+    util.collate('image', 2*[util.prepare_input('image', im_data, {}, {})]),
+    util.collate('detection', 2*[util.prepare_input('detection', detection_data, {'CanvasDims': [300, 300]}, {})]),
+]
+util.inputs_to_device(example_inputs, device)
+
+# example metadata
+categories = [
+    'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat',
+    'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person',
+    'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor',
+]
+example_metadatas = [{}, {'Categories': categories}]
+
+net = model.Net(arch, comps, example_inputs, example_metadatas, device=devic
