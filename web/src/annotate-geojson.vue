4e29c67d6ce29c605b23f6ffc4f2c12011a20d66
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-10-01 16:20:31 +0000

    travis build status

--- web/src/annotate-geojson.vue
+++ web/src/annotate-geojson.vue
@@ -7,4 +7,52 @@
 					<label class="mx-1">Tile URL</label>
 				</div>
 				<div class="col-auto">
-					<input type="text" class="
+					<input type="text" class="form-control mx-1" v-model="params.TileURL" />
+				</div>
+				<div class="col-auto">
+					<button type="submit" class="btn btn-primary mx-1">Save Settings</button>
+				</div>
+			</form>
+		</div>
+		<div ref="map" style="width: 100%; height: 100%"></div>
+		<div class="mt-2">
+			<button type="button" class="btn btn-primary mx-1" v-on:click="saveFeatures">Save GeoJSON</button>
+		</div>
+	</template>
+</div>
+</template>
+
+<script>
+import utils from './utils.js';
+import LatLonProvider from './geojson/leaflet-geosearch-provider.js';
+
+export default {
+	data: function() {
+		return {
+			// The annotation dataset, which must be GeoJSON type.
+			annoset: null,
+
+			// Tool parameters.
+			params: {
+				// The Leaflet.js URL template from which images should be fetched.
+				// See https://leafletjs.com/reference-1.7.1.html#tilelayer
+				TileURL: '',
+			},
+
+			// Current GeoJSON FeatureCollection.
+			// On initialization: this comes from the "geojson" key in annoset.
+			// Afterwards (on save): features extracted from the Leaflet instance.
+			featureCollection: {
+				type: "FeatureCollection",
+				features: [],
+			},
+
+			// Constant item key.
+			// For GeoJSON data type, we put all features into one item with this key.
+			itemKey: 'geojson',
+
+			// Currently initialized Leaflet map, if any.
+			map: null,
+		};
+	},
+	
