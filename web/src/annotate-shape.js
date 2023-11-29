d7122b47f9d89d3a1650e7552259540b84481bac
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2023-11-29 09:42:10 +0000

    Updated package dependencies

--- web/src/annotate-shape.js
+++ web/src/annotate-shape.js
@@ -70,3 +70,53 @@ export default AnnotateGenericUI({
 		this.shapes = data.map((shapeList) => {
 			return shapeList.map((shp) => this.decodeShape(shp));
 		});
+
+		// update if we already rendered image
+		if(this.imageDims != null) {
+			this.render();
+		}
+	},
+	on_image_loaded: function() {
+		Vue.nextTick(() => {
+			this.render();
+		});
+	},
+	getAnnotateData: function() {
+		let data = this.shapes.map((shapeList) => {
+			return shapeList.map((shape) => this.encodeShape(shape))
+		});
+		let metadata = {
+			CanvasDims: [this.imageDims.Width, this.imageDims.Height],
+			Categories: this.params.Categories,
+		};
+		return [data, metadata];
+	},
+	methods: {
+		disconnectResizeObserver: function() {
+			if(this.resizeObserver) {
+				this.resizeObserver.disconnect();
+				this.resizeObserver = null;
+			}
+		},
+		decodeShape: function(shape) {
+			let shp = {};
+			if(this.dataType === 'shape') {
+				shp.Type = shape.Type;
+				shp.Points = shape.Points;
+			} else if(this.dataType === 'detection') {
+				shp.Type = 'box';
+				shp.Points = [[shape.Left, shape.Top], [shape.Right, shape.Bottom]];
+			}
+			shp.Category = (shape.Category) ? shape.Category : '';
+			shp.TrackID = (shape.TrackID) ? shape.TrackID : '';
+			return shp;
+		},
+		encodeShape: function(shape) {
+			let shp = {};
+			if(this.dataType === 'shape') {
+				shp.Type = shape.Type;
+				shp.Points = shape.Points;
+			} else if(this.dataType === 'detection') {
+				shp.Left = shape.Points[0][0];
+				shp.Top = shape.Points[0][1];
+				shp.Right = shape.Points[1][0];
