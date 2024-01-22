299f43487da764fac10305419d662fe451ff6250
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2024-01-22 11:18:28 +0000

    Update package.json fields

--- web/src/exec-edit/pytorch-restore.vue
+++ web/src/exec-edit/pytorch-restore.vue
@@ -25,4 +25,60 @@
 			<div class="col-sm-8">
 				<input v-model.number="restore[idx].SkipPrefixes" type="text" class="form-control" @change="update">
 				<small class="form-text text-muted">
-					Comma-separa
+					Comma-separated list of prefixes that should not be restored.
+				</small>
+			</div>
+		</div>
+	</template>
+</div>
+</template>
+
+<script>
+import utils from '../utils.js';
+
+export default {
+	data: function() {
+		return {
+			restore: [],
+			parentNames: [],
+		};
+	},
+	props: ['node', 'value'],
+	created: function() {
+		if(this.value) {
+			this.restore = this.value;
+		}
+
+		let models = [];
+		if(this.node.Parents && this.node.Parents['models']) {
+			models = this.node.Parents['models'];
+		}
+		while(this.restore.length < models.length) {
+			this.restore.push({
+				SrcPrefix: '',
+				DstPrefix: '',
+				SkipPrefixes: '',
+			});
+		}
+		this.parents = [];
+		models.forEach((parent, idx) => {
+			this.parentNames.push('unknown');
+			if(parent.Type == 'n') {
+				utils.request(this, 'GET', '/exec-nodes/'+parent.ID, null, (node) => {
+					this.$set(this.parentNames, idx, node.Name);
+				});
+			} else if(parent.Type == 'd') {
+				utils.request(this, 'GET', '/datasets/'+parent.ID, null, (ds) => {
+					this.$set(this.parentNames, idx, ds.Name);
+				});
+			}
+		});
+		this.update();
+	},
+	methods: {
+		update: function() {
+			this.$emit('input', this.restore);
+		},
+	},
+};
+</script>
---
+++ web/src/exec-edit/pytorch-train.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="small-container">
+	<h3>Basics</h3>
+	<div class="form-group row">
+		<label class="col-sm-4 col-form-label">Learning Rate</label>
+		<div class="col-sm-8">
+			<input v-model.number="p.LearningRate" type="text" clas
