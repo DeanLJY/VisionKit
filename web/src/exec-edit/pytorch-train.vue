f310da77589b9ad68277ab192bf941681066f10d
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2024-01-22 15:12:47 +0000

    queue

--- web/src/exec-edit/pytorch-train.vue
+++ web/src/exec-edit/pytorch-train.vue
@@ -44,4 +44,36 @@
 		</div>
 	</div>
 	<div class="form-group row">
-		
+		<label class="col-sm-4 col-form-label">Epochs Without Improvement</label>
+		<div class="col-sm-8">
+			<input v-model.number="p.StopCondition.ScoreMaxEpochs" type="text" class="form-control" @change="update">
+			<small class="form-text text-muted">
+				Stop training if this many epochs have elapsed without non-negligible improvement in the score. Leave 0 to disable this stop condition.
+			</small>
+		</div>
+	</div>
+	<div class="form-group row">
+		<label class="col-sm-4 col-form-label">Improvement Threshold</label>
+		<div class="col-sm-8">
+			<input v-model.number="p.StopCondition.ScoreEpsilon" type="text" class="form-control" @change="update">
+			<small class="form-text text-muted">
+				Increases in the score less than this threshold are considered negligible. 0 implies that any increase will reset the timer for Epochs Without Improvement.
+			</small>
+		</div>
+	</div>
+
+	<h3>Model Saver</h3>
+	<div class="form-group row">
+		<label class="col-sm-4 col-form-label">Saver Mode</label>
+		<div class="col-sm-8">
+			<select v-model="p.ModelSaver.Mode" class="form-select" @change="update">
+				<option value="latest">Save the latest model</option>
+				<option value="best">Save the model with best validation score</option>
+			</select>
+		</div>
+	</div>
+
+	<h3>Rate Decay</h3>
+	<div class="form-group row">
+		<label class="col-sm-4 col-form-label">Rate Decay Mode</label>
+		<div class="col-sm-
