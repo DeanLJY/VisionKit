2ae5c9e7124356f8e9126c1e31ae2b1f2755dc42
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2024-02-02 06:15:32 +0000

    crypto: msm: change dev_err messages to dev_warn for reaper worker

--- web/src/exec-edit/pytorch_unet_train.js
+++ web/src/exec-edit/pytorch_unet_train.js
@@ -3,4 +3,44 @@ import SelectInputSize from './select-input-size.vue';
 
 export default PytorchTrainGeneric({
 	components: {
-		'select-input-size': 
+		'select-input-size': SelectInputSize,
+	},
+	disabled: ['model', 'dataset'],
+	created: function() {
+		if(!('NumClasses' in this.params)) this.$set(this.params, 'NumClasses', 0);
+		if(!('ValPercent' in this.params)) this.$set(this.params, 'ValPercent', 20);
+		if(!('Resize' in this.params)) {
+			this.$set(this.params, 'Resize', {
+				Mode: 'keep',
+				MaxDimension: 640,
+				Width: 256,
+				Height: 256,
+				Multiple: 8,
+			});
+		}
+	},
+	basicTemplate: `
+<div class="small-container">
+	<div class="row mb-2">
+		<label class="col-sm-4 col-form-label">Number of Classes</label>
+		<div class="col-sm-8">
+			<input v-model.number="params.NumClasses" type="text" class="form-control">
+			<small class="form-text text-muted">
+				The number of segmentation categories.
+			</small>
+		</div>
+	</div>
+	<div class="row mb-2">
+		<label class="col-sm-4 col-form-label">Validation Percentage</label>
+		<div class="col-sm-8">
+			<input v-model.number="params.ValPercent" type="text" class="form-control">
+			<small class="form-text text-muted">
+				Use this percentage of the input data for validation. The rest will be used for training.
+			</small>
+		</div>
+	</div>
+	<hr />
+	<select-input-size v-model="params.Resize"></select-input-size>
+</div>
+	`,
+});
---
+++ web/src/exec-edit/pytorch_yolov3_infer.vue
@@ -0,0 +1,4 @@
+<template>
+<div class="small-container m-2">
+	<template v-if="node != null">
+		<select-input-size v-model="params.Resize"></select-inpu
