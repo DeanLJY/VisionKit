0a67be4b78618f8bb8424b5c01729f9be78536d6
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2024-02-08 21:06:15 +0000

    Delete docs

--- web/src/exec-edit/pytorch_yolov5_train.js
+++ web/src/exec-edit/pytorch_yolov5_train.js
@@ -22,4 +22,30 @@ export default PytorchTrainGeneric({
 	basicTemplate: `
 <div class="small-container">
 	<div class="row mb-2">
-		<label class="col-sm-4 col-form-label">Mode</lab
+		<label class="col-sm-4 col-form-label">Mode</label>
+		<div class="col-sm-8">
+			<select v-model="params.Mode" class="form-select">
+				<option value="s">YOLOv5s</option>
+				<option value="m">YOLOv5m</option>
+				<option value="l">YOLOv5l</option>
+				<option value="x">YOLOv5x</option>
+			</select>
+			<small class="form-text text-muted">
+				Choose a model size: small (s), medium (m), large (l), and x-large (x).
+			</small>
+		</div>
+	</div>
+	<div class="row mb-2">
+		<label class="col-sm-4 col-form-label">Validation Percentage</label>
+		<div class="col-sm-8">
+			<input v-model.number="params.ValPercent" type="text" class="form-control">
+			<small class="form-text text-muted">
+				Use this percentage of the input data for validation. The rest will be used for training.
+			</small>
+		</div>
+	</div>
+	<hr />
+	<select-input-size v-model="params.Resize"></select-input-size>
+</div>
+	`,
+});
---
+++ web/src/exec-edit/resample.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="small-container m-2">
+	<template v-if="node != null">
+		<div class="form-group row">
+			<label class="col-sm-2 col-form-label">Fraction</label>
+			<div class="col-sm-10">
+				<input v-model="fraction" type="text" class="form-control">
+				<small class="form-text text-muted">
+					Re-sample input data by this fraction
