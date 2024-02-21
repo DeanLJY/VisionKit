62b0c721a20b4d8e4c44cf7fd96db592c7e718e5
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2024-02-21 06:22:36 +0000

    Prepare 0.2.4 release

--- web/src/exec-edit/select-input-size.vue
+++ web/src/exec-edit/select-input-size.vue
@@ -6,4 +6,28 @@ Offers three resolution modes which should be stored in some parameter object.
 <template>
 <div class="small-container">
 	<div class="form-group row">
-		<
+		<label class="col-sm-4 col-form-label">Resize Mode</label>
+		<div class="col-sm-8">
+			<div class="form-check">
+				<input class="form-check-input" type="radio" v-model="params.Mode" value="scale-down" @change="update">
+				<label class="form-check-label">Scale-Down: Downsize image so that its maximum dimension is at most a configurable value.</label>
+			</div>
+			<div class="form-check">
+				<input class="form-check-input" type="radio" v-model="params.Mode" value="fixed" @change="update">
+				<label class="form-check-label">Fixed: Resize to a fixed resolution.</label>
+			</div>
+			<div class="form-check">
+				<input class="form-check-input" type="radio" v-model="params.Mode" value="keep" @change="update">
+				<label class="form-check-label">Keep: Use input images as-is without resizing.</label>
+			</div>
+		</div>
+	</div>
+	<template v-if="params.Mode == 'scale-down'">
+		<div class="form-group row">
+			<label class="col-sm-4 col-form-label">Max Dimension</label>
+			<div class="col-sm-8">
+				<input v-model.number="params.MaxDimension" type="text" class="form-control" @change="update">
+				<small class="form-text text-muted">
+					Scale down images so that their maximum dimension is at most this value.
+					For example, if set to 640, a 1280x720 input image would be scaled to 640x360.
+				</sma
