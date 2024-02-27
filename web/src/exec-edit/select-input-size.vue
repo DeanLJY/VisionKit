37700a065cbda84f68717ab076d339000989642e
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2024-02-27 07:12:13 +0000

    Update NOTE.md

--- web/src/exec-edit/select-input-size.vue
+++ web/src/exec-edit/select-input-size.vue
@@ -30,4 +30,53 @@ Offers three resolution modes which should be stored in some parameter object.
 				<small class="form-text text-muted">
 					Scale down images so that their maximum dimension is at most this value.
 					For example, if set to 640, a 1280x720 input image would be scaled to 640x360.
-				</sma
+				</small>
+			</div>
+		</div>
+	</template>
+	<template v-if="params.Mode == 'fixed'">
+		<div class="form-group row">
+			<label class="col-sm-4 col-form-label">Width</label>
+			<div class="col-sm-8">
+				<input v-model.number="params.Width" type="text" class="form-control" @change="update">
+				<small class="form-text text-muted">
+					Resize the image to this width.
+				</small>
+			</div>
+		</div>
+		<div class="form-group row">
+			<label class="col-sm-4 col-form-label">Height</label>
+			<div class="col-sm-8">
+				<input v-model.number="params.Height" type="text" class="form-control" @change="update">
+				<small class="form-text text-muted">
+					Resize the image to this height.
+				</small>
+			</div>
+		</div>
+	</template>
+	<div class="form-group row">
+		<label class="col-sm-4 col-form-label">Multiple</label>
+		<div class="col-sm-8">
+			<input v-model.number="params.Multiple" type="text" class="form-control" @change="update">
+			<small class="form-text text-muted">
+				Ensure the final image resolution is a multiple of this number on both dimensions, by rounding down.
+			</small>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+import utils from '../utils.js';
+
+export default {
+	data: function() {
+		return {
+			params: {},
+		};
+	},
+	props: ['value'],
+	created: function() {
+		let params = {};
+		if(this.value) {
+			for(let [k, v] of Objec
