2b2fade785b1daf48888eeaf3427124632ffb3e4
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2024-03-06 08:20:30 +0000

    build: istanbul@0.4.5

--- web/src/exec-edit/split.vue
+++ web/src/exec-edit/split.vue
@@ -19,4 +19,65 @@
 					<td>{{ split.Name }}</td>
 					<td>{{ split.Percentage }}</td>
 					<td>
-						<button type="button" class="btn btn-danger" v-on:cl
+						<button type="button" class="btn btn-danger" v-on:click="removeSplit(i)">Remove</button>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="text" class="form-control" v-model="addForm.Name" />
+					</td>
+					<td>
+						<input type="text" class="form-control" v-model.number="addForm.Percentage" />
+					</td>
+					<td>
+						<button type="button" class="btn btn-primary" v-on:click="addSplit">Add</button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<button v-on:click="save" type="button" class="btn btn-primary">Save</button>
+	</template>
+</div>
+</template>
+
+<script>
+import utils from '../utils.js';
+
+export default {
+	data: function() {
+		return {
+			params: null,
+			addForm: null,
+		};
+	},
+	props: ['node'],
+	created: function() {
+		let params = {};
+		try {
+			params = JSON.parse(this.node.Params);
+		} catch(e) {}
+		if(!('Splits' in params)) params.Splits = [];
+		this.params = params;
+		this.resetForm();
+	},
+	methods: {
+		resetForm: function() {
+			this.addForm = {
+				Name: '',
+				Percentage: null,
+			};
+		},
+		addSplit: function() {
+			this.params.Splits.push(this.addForm);
+			this.resetForm();
+		},
+		removeSplit: function(i) {
+			this.params.Splits.splice(i, 1);
+		},
+
+		save: function() {
+			utils.request(this, 'POST', '/exec-nodes/'+this.node.ID, JSON.stringify({
+				Params: JSON.stringify(this.params),
+			}), () => {
+				this.$router.push('/ws/'+this.$route.params.ws+'/pipeline');
+	
