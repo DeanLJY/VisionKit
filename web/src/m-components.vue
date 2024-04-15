e892e63d5a03c22df93cfd3b0e039d6b5faf6d5a
DeanLJY <100091478+DeanLJY@users.noreply.github.com>
2024-04-15 07:41:29 +0000

    Changed files

--- web/src/m-components.vue
+++ web/src/m-components.vue
@@ -2,4 +2,60 @@
 <div>
 	<button type="button" class="btn btn-primary my-1" v-on:click="showAddModal">Add Component</button>
 	<div class="modal" tabindex="-1" role="dialog" ref="addModal">
-		<div class="modal-
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-body">
+					<form v-on:submit.prevent="add">
+						<div class="row mb-2">
+							<label class="col-sm-4 col-form-label">ID</label>
+							<div class="col-sm-8">
+								<input class="form-control" type="text" v-model="addForm.id" required />
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-8">
+								<button type="submit" class="btn btn-primary">Add Component</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<table class="table table-sm">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="comp in comps">
+				<td>{{ comp.ID }}</td>
+				<td>
+					<router-link :to="'/ws/'+$route.params.ws+'/models/comp/'+comp.ID" class="btn btn-sm btn-primary">Manage</router-link>
+					<button v-on:click="deleteComp(comp.ID)" class="btn btn-sm btn-danger">Delete</button>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+</template>
+
+<script>
+import utils from './utils.js';
+
+export default {
+	data: function() {
+		return {
+			comps: [],
+			addForm: {},
+		};
+	},
+	props: ['mtab'],
+	created: function() {
+		this.fetch();
+	},
+	methods: {
+		fetch: function() {
+			utils.request(this, 'GET', '/pytorch/components'
